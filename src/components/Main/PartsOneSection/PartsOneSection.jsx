b50c41f7a1f24a7e0faf677b3f0f6f6ff69f1374
Matheus <tetisouto@gmail.com>
2025-07-03 17:18:34 -0300

    att

--- src/components/Main/PartsOneSection/PartsOneSection.jsx
+++ src/components/Main/PartsOneSection/PartsOneSection.jsx
@@ -64,7 +64,7 @@ function PartsOneSection() {
       <div className={styles.mainPartsOne}>
         <div id="pecas" className={styles.containerPartsOne}>
           <div className={styles.titleParts}>
-            <h2>Teste</h2>
+            <h2>Nossas peças em destaque</h2>
           </div>
           <div className={styles.sliderPartsOne}>
             <div ref={prevRef} className={styles.arrowLeft}>
